e917db146479efbeb91df96740b0ca36df297043
chunxiao ye <yechunxiao19@gmail.com>
2013-12-21 14:00:37 +0800

    init

---
+++ posimarker/posimarker/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  posimarker
+//
+//  Created by yechunxiao on 12/21/13.
+//  Copyright (c) 2013 posi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
